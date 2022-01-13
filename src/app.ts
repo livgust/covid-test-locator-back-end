dba63762561bf92c5711443c3f7abe1a9bb86291
Olivia Adams <liv.gust@gmail.com>
2022-01-12 21:55:39 -0500

    feat: complete google search functionality

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, {json, urlencoded} from 'express';
 import googlePassThrough from './routes/googlePassThrough';
 import placesRouter from './routes/places';
 import {runMigrations} from './database/migrations';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(json());
 app.use(urlencoded({extended: true}));
 
---
+++ src/mappers/googlePlacesToPlaces.ts
@@ -0,0 +1,70 @@
+import db from '../database/models';
+import {Place} from '../types';
+import {mapDbPlaceToPlace} from './dataMapping';
+
+export const mergeGooglePlacesWithPlaces = async (
+  googlePlaces: google.maps.places.PlaceResult[]
+): Promise<Place[]> => {
+  const locations: Place['location'][] = [];
+  const googlePlaceIds: string[] = [];
+  const mappedGooglePlaces = googlePlaces.map(
+    ({geometry, name, place_id, vicinity}) => {
+      const location = {
+        lat: geometry!.location!.lat as unknown as number,
+        long: geometry!.location!.lng as unknown as number,
+      };
+      locations.push(location);
+      googlePlaceIds.push(place_id!);
+      return {
+        location,
+        name,
+        googlePlaceId: place_id,
+        vicinity,
+      } as Place;
+    }
+  );
+  const foundPlaces = await searchForPlacesWithLocationsAndGooglePlaceIds({
+    locations,
+    googlePlaceIds,
+  });
+  const foundPlaceGooglePlaceIdMap: {[key: string]: Place} = {};
+  const foundPlaceLocationMap: {[key: string]: Place} = {};
+  foundPlaces.forEach(foundPlace => {
+    foundPlaceGooglePlaceIdMap[foundPlace.googlePlaceId] = foundPlace;
+    const locationString = JSON.stringify(foundPlace.location);
+    foundPlaceLocationMap[locationString] = foundPlace;
+  });
+
+  const mergedPlaces = mappedGooglePlaces.map(mappedGooglePlace => {
+    const locationString = JSON.stringify(mappedGooglePlace.location);
+    const foundPlace =
+      foundPlaceGooglePlaceIdMap[mappedGooglePlace.googlePlaceId] ||
+      foundPlaceLocationMap[locationString];
+    return foundPlace || mappedGooglePlace;
+  });
+  return mergedPlaces;
+};
+
+export const searchForPlacesWithLocationsAndGooglePlaceIds = async ({
+  locations,
+  googlePlaceIds,
+}: {
+  locations: {lat: number; long: number}[];
+  googlePlaceIds: string[];
+}): Promise<Place[]> => {
+  const results = await db.Place.findAll({
+    where: {
+      [db.Sequelize.Op.or]: [
+        {
+          googlePlaceId: {
+            [db.Sequelize.Op.or]: googlePlaceIds,
+          },
+          location: {
+            [db.Sequelize.Op.or]: locations,
+          },
+        },
+      ],
+    },
+  });
+  return results.map(mapDbPlaceToPlace);
+};
